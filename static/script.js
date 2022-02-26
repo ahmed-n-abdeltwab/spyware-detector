598bb59271c08bc3798e3293b439aa5e4c5c51bd
Ahmed-nd <ahmed.n.abdeltwab@gmail.com>
2022-02-26 14:09:26 +0200

    v1.0

---
+++ static/script.js
@@ -0,0 +1,46 @@
+function checkImageFile() {
+    document.querySelector('#fileInputButton').submit();
+    console.log( document.querySelector('#fileInputButton'))
+}
+document.onpaste = function(event){
+	urlInput = document.getElementById("urlInput");
+	if(event.target == urlInput){
+		//don't interfere with paste to url box - allows ios to paste image links properly
+		//give some time for paste to finish normally before checking
+		setTimeout(function(){ getURLInput(urlInput); }, 4);
+		return;
+	}else{
+		event.preventDefault();
+		clipboardData = (event.clipboardData || event.originalEvent.clipboardData);
+		if(typeof clipboardData.files[0] == 'undefined'){
+			urlInput.value = clipboardData.getData('Text');
+			getURLInput(urlInput);
+		}else{
+			fileInput = document.getElementById("fileInput");
+			fileInput.files = clipboardData.files;
+			checkImageFile(fileInput);
+		}
+	}
+}
+document.ondragover = document.ondragenter = function(event) {
+	event.preventDefault();
+};
+document.ondrop = function(event){
+	fileInput = document.getElementById("fileInput");
+	if(event.target == fileInput){
+		//don't interfere with drop on file select - allows old browsers to drop properly
+		//console.log("skipped drop!");
+		return;
+	}else{
+		event.preventDefault();
+		if(typeof event.dataTransfer.files[0] == 'undefined'){
+			urlInput = document.getElementById("urlInput");
+			urlInput.value = event.dataTransfer.getData("text/uri-list");
+			getURLInput(urlInput);
+		}else{
+			fileInput = document.getElementById("fileInput");
+			fileInput.files = event.dataTransfer.files;
+			checkImageFile(fileInput);
+		}
+	}
+}

65705aefdf063a87d99ee1b71cd7a1a4206c732e
Ahmed Nasser <62723180+Ahmed-nd@users.noreply.github.com>
2022-05-06 16:47:06 +0200

    Delete Data Modeling directory

--- Data Modeling/ClassifyKNN.py
+++
@@ -1,6 +0,0 @@
-def classify(features_train, labels_train): 
-    from sklearn.neighbors import KNeighborsClassifier
-
-    knn = KNeighborsClassifier(n_neighbors = 35)
-    knn.fit(features_train , labels_train)
-    return knn

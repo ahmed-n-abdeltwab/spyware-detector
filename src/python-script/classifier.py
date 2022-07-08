940828c8652fda414ce21f891da09550ec3e1f78
ahmednss <ahmed.n.abdeltwab@gmail.com>
2022-07-08 06:11:24 +0200

    fixing : bugs

--- src/python-script/classifier.py
+++ src/python-script/classifier.py
@@ -12,33 +12,34 @@ def Classifier(scannerResult):
     filter_arr = list(joblib.load("./SUPPORT.joblib"))
     features = features[filter_arr]
 
-    # Features Scaling
-    Sclfeatures = MinMaxScaler().fit_transform(features.reshape(len(features), -1))
-    Sclfeatures = Sclfeatures.reshape(1, -1)
+    features = features.reshape(1, -1)
+
     # load the model
     model = joblib.load("./LOGISTIC_REGRESSION_MODEL.joblib")
 
     # get the prediction
-    prediction = int(model.predict(Sclfeatures)[0])
-    pred_pro = list(model.predict_proba(Sclfeatures)[0])
-    if prediction == 0:
+    prediction = int(model.predict(features)[0])
+    pred_pro = list(model.predict_proba(features)[0])
+    APIs = usedAPIs(API_list)
+    if prediction == 0 and len(APIs) > 0:
         return {
             "prediction": prediction,
-            "details": {"prob": pred_pro, "apiList": topReason(API_list)},
+            "details": {"prob": pred_pro, "apiList": APIs},
         }
     return {"prediction": prediction, "details": {"prob": pred_pro}}
 
 
-def topReason(API_list):
+def usedAPIs(API_list):
     import pandas as pd
-
-    df = pd.read_html("https://malapi.io/", attrs={"id": "main-table"})
-    Spying = (df[0]["Spying"][0]).split()
-    Evasion = (df[0]["Evasion"][0]).split()
-    malapiList = Lower(Spying + Evasion)
-    API_list = Lower(API_list)
-
-    return list(filter(lambda api: api in API_list, malapiList))
+    try:
+        df = pd.read_html("https://malapi.io/", attrs={"id": "main-table"})
+        Spying = (df[0]["Spying"][0]).split()
+        Evasion = (df[0]["Evasion"][0]).split()
+        malapiList = Lower(Spying + Evasion)
+        API_list = Lower(API_list)
+        return list(filter(lambda api: api in API_list, malapiList))
+    except:
+        return []
 
 
 def Lower(l: list):

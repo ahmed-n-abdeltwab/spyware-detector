f0231055860d0ea55cfcc930aa9cb38fed5d69b3
ahmednss <ahmed.n.abdeltwab@gmail.com>
2022-07-08 06:18:55 +0200

    fixing : some styleing

--- src/python-script/classifier.py
+++ src/python-script/classifier.py
@@ -20,11 +20,10 @@ def Classifier(scannerResult):
     # get the prediction
     prediction = int(model.predict(features)[0])
     pred_pro = list(model.predict_proba(features)[0])
-    APIs = usedAPIs(API_list)
-    if prediction == 0 and len(APIs) > 0:
+    if prediction == 0:
         return {
             "prediction": prediction,
-            "details": {"prob": pred_pro, "apiList": APIs},
+            "details": {"prob": pred_pro, "apiList": usedAPIs(API_list)},
         }
     return {"prediction": prediction, "details": {"prob": pred_pro}}
 
@@ -32,14 +31,14 @@ def Classifier(scannerResult):
 def usedAPIs(API_list):
     import pandas as pd
     try:
-        df = pd.read_html("https://malapi.io/", attrs={"id": "main-table"})
+        df = pd.read_html("ht0tps://malapi.io/", attrs={"id": "main-table"})
         Spying = (df[0]["Spying"][0]).split()
         Evasion = (df[0]["Evasion"][0]).split()
         malapiList = Lower(Spying + Evasion)
         API_list = Lower(API_list)
         return list(filter(lambda api: api in API_list, malapiList))
     except:
-        return []
+        return API_list
 
 
 def Lower(l: list):
